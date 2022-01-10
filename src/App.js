f5a347dca827cceb7a61fa45598b83377a436996
zack <zack>
2022-01-09 17:59:12 -0800

    Git push testing 5

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import LatticeAnimalDemo from './components/LatticeAnimalDemo.js';
 function App() {
   return (
       <div className="App" style={{'backgroundColor':'#e0e0e0'}}>
-          <h1>Lattice Animals</h1>
+          <h1>Lattice Animals are cool</h1>
         Hello and welcome to latticeanimal.com. This website is devoted to fun (and often math related) websites that I want to create. It is named after the mathematical construct of a lattice animal (also known as a Polyomino <a href="https://en.wikipedia.org/wiki/Polyomino">wikipedia.org/wiki/Polyomino</a>). 
         <br/>
         <br/>
