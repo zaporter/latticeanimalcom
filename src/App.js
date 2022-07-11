4c342019db7cb86c47a59fe1b949c97c06b4e22e
zack <zack>
2022-07-11 17:42:17 +0200

    Make it look pretty

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import LatticeAnimalDemo from './components/LatticeAnimalDemo.js';
+import './App.css';
 
 function App() {
   return (
-      <div className="App" style={{'backgroundColor':'#e0e0e0'}}>
-          <h1>Lattice Animals are cool</h1>
-          <h1>I really need to update the css of this page..</h1>
+    <div className="App" >
+      <h1 className='heading'>Lattice Animals Are Cool</h1>
 
+      <div className='item'> 
         Hello and welcome to latticeanimal.com. This website is devoted to fun (and often math related) websites that I want to create. It is named after the mathematical construct of a lattice animal (also known as a Polyomino <a href="https://en.wikipedia.org/wiki/Polyomino">wikipedia.org/wiki/Polyomino</a>). 
         <br/>
         <br/>
         Here is little demo allowing you see different orders (sizes) of different polyominoes and their respective type (or uniqueness criterion).  
-
-        I have constrainted the maximum and minimum size because while I am using the Redelmeier method for fixed polomino generation, the free and one-sided checks are compute intensive and javascript isn't fast.
+        <div style={{'display':'flex','justifyContent':'center'}}>
+          <p>
         <LatticeAnimalDemo/>
+          </p>
+          </div>
+      </div>
+      <h1 className='heading'>Projects</h1>
+      
+      <div className='item'>
+        <a href="https://www.latticeanimal.com/center-of-the-world/"><h1> Azimuthal Equidistant Projection (in Rust!)</h1> </a>
+      </div>
+      <div className='item'>
+        <a href="https://latticeanimal.com/distributionexplorer/"><h1> Distribution Explorer</h1></a>
+      </div>
+      <div className='item'>
+        <a href="https://www.latticeanimal.com/mastermind/"><h1> Mastermind Solver</h1> </a>
+      </div>
+      <h1 className='heading'> About this site</h1>
+      <div className='item'>
+        If you have any questions or concerns, please contact me on github or send me an email: general@latticeanimal.com (this email may or may not be set up yet.... )
+        </div>
+      <br/>
     </div>
   );
 }
